5e48db1b7f6006f19c40825e7f686069e9b270a7
Steve Schwab <schwabs@umich.edu>
2024-03-26 07:26:07 -0700

    create quote slice with initial state

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,28 +1,32 @@
-// ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit"
 
 let id = 1
 const getNextId = () => id++
-const initialState = {
-  displayAllQuotes: true,
-  highlightedQuote: null,
-  quotes: [
-    {
-      id: getNextId(),
-      quoteText: "Don't cry because it's over, smile because it happened.",
-      authorName: "Dr. Seuss",
-      apocryphal: false,
-    },
-    {
-      id: getNextId(),
-      quoteText: "So many books, so little time.",
-      authorName: "Frank Zappa",
-      apocryphal: false,
-    },
-    {
-      id: getNextId(),
-      quoteText: "Be yourself; everyone else is already taken.",
-      authorName: "Oscar Wilde",
-      apocryphal: false,
-    },
-  ],
-}
+
+const slice = createSlice({
+  name: 'quote_state',
+  initialState: {
+    displayAllQuotes: true,
+    highlightedQuote: null,
+    quotes: [
+      {
+        id: getNextId(),
+        quoteText: "Don't cry because it's over, smile because it happened.",
+        authorName: "Dr. Seuss",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "So many books, so little time.",
+        authorName: "Frank Zappa",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "Be yourself; everyone else is already taken.",
+        authorName: "Oscar Wilde",
+        apocryphal: false,
+      },
+    ],
+  }
+})
